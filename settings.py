682dc4e242bfe2a55ce67dcfab8d21fbbe999e8a
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2024-01-24 12:49:02 -0800

    create bullet class

--- settings.py
+++ settings.py
@@ -4,3 +4,8 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (50, 10, 77)
         self.ship_speed = 5.0
+
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
