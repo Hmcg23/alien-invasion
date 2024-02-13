f26caa1954aeef68413c74cec8788095af0c1961
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2024-02-13 14:36:10 -0800

    add high score functionality

--- game_stats.py
+++ game_stats.py
@@ -3,6 +3,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
         self.score = 0
+
+        # Do not reset score
+        self.high_score = 0
     
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
